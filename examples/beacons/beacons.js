f26af81c130e9897b90b832f3ce8033cd86fa4c8
David Caplan <dcaplan@gmail.com>
2017-04-07 17:59:09 -0400

    change indents to 2sp

--- examples/beacons/beacons.js
+++ examples/beacons/beacons.js
@@ -1,55 +1,59 @@
 SolveBio.init({
-    accessToken: 'ACCESS TOKEN'
+  accessToken: 'ACCESS TOKEN'
 });
 
 // Beacon datasets given form input
 var form = $("#form-beacons");
 
 form.submit(function(event) {
-    var query = form.find('input[name=query]').val();
-    var visibility = form.find('select[name=visibility] option:selected').val();
-    var output = $("#form-beacons-output");
-    output.html('');
+  var query = form.find('input[name=query]').val();
+  var visibility = form.find('select[name=visibility] option:selected').val();
+  var output = $("#form-beacons-output");
+  output.html('');
 
-    SolveBio.Expression('entity_ids("variant", query)', 'string', true)
+  // Pass the query through the entity_ids() function which
+  // will find one or more variant IDs equivalent to the query.
+  SolveBio.Expression('entity_ids("variant", query)', 'string', true)
+    .evaluate({
+      query: query
+    })
+    .then(function(response) {
+      // NOTE: There may be multiple entity IDs returned for a variant.
+      var variant = _.get(response, 'result.0');
+
+      if (!variant) {
+        output.append('Variant could not be normalized</br>');
+        return;
+      }
+
+      output.append('Normalized variant IDs: </br>');
+      output.append(_.get(response, 'result').join("</br>"));
+      output.append('</br></br>');
+
+      // Runs a beacon query for a single variant against all
+      // available public or private datasets.
+      return SolveBio.Expression('beacon(variant, "variant", visibility=visibility)', 'object', false)
         .evaluate({
-            query: query
-        })
-        .then(function(response) {
-            // NOTE: There may be multiple entity IDs returned for a variant.
-            var variant = _.get(response, 'result.0');
-
-            if (!variant) {
-                output.append('Variant could not be normalized</br>');
-                return;
-            }
-
-            output.append('Normalized variant IDs: </br>');
-            output.append(_.get(response, 'result').join("</br>"));
-            output.append('</br></br>');
-
-            return SolveBio.Expression('beacon(variant, "variant", visibility=visibility)', 'object', false)
-                .evaluate({
-                    variant: variant,
-                    visibility: visibility
-                });
-        })
-        .then(function(response) {
-            if(response) {
-                var datasets = _.get(response, 'result.found');
-
-                if(_.get(datasets, 'length')) {
-                    output.append("Found " + datasets.length + " beacons:</br>");
-                    output.append(datasets.join("</br>"));
-                }
-                else {
-                    output.append("No beacons matched the variant.");
-                }
-            }
-        })
-        .catch(function(response) {
-            output.text('Error: ' + _.get(response, 'detail'));
+          variant: variant,
+          visibility: visibility
         });
+    })
+    .then(function(response) {
+      if(response) {
+        var datasets = _.get(response, 'result.found');
 
-    event.preventDefault();
+        if(_.get(datasets, 'length')) {
+          output.append("Found " + datasets.length + " beacons:</br>");
+          output.append(datasets.join("</br>"));
+        }
+        else {
+          output.append("No beacons matched the variant.");
+        }
+      }
+    })
+    .catch(function(response) {
+      output.text('Error: ' + _.get(response, 'detail'));
+    });
+
+  event.preventDefault();
 });
--- examples/simple/simple.js
+++ examples/simple/simple.js
@@ -1,33 +1,33 @@
 SolveBio.init({
-    accessToken: 'ACCESS TOKEN'
+  accessToken: 'ACCESS TOKEN'
 });
 
 // Try querying ClinVar for 10 records
 var dataset = SolveBio.Dataset('ClinVar/Variants');
 dataset.query({limit: 10})
-    .then(function(response) {
-        var pre = document.getElementById('clinvar');
-        pre.innerHTML = '';
-        pre.innerHTML += 'RCV Accession,Variant,Phenotype<br/>';
-        response.results.forEach(function(row) {
-            pre.innerHTML += row.rcv_accession + ',' + row.variant_sbid + ',' + row.phenotype + '<br/>';
-        });
+  .then(function(response) {
+    var pre = document.getElementById('clinvar');
+    pre.innerHTML = '';
+    pre.innerHTML += 'RCV Accession,Variant,Phenotype<br/>';
+    response.results.forEach(function(row) {
+      pre.innerHTML += row.rcv_accession + ',' + row.variant_sbid + ',' + row.phenotype + '<br/>';
     });
+  });
 
 // Try running Veppy (variant effect prediction)
 // NOTE: Requires a valid accessToken
 var expr = SolveBio.Expression('predict_variant_effects(variant)', 'object', true);
 expr.evaluate({variant: 'GRCH37-7-140453136-140453136-T'})
-    .then(function(response) {
-        var pre = document.getElementById('effects');
-        pre.innerHTML = '';
-        pre.innerHTML += 'Transcript,Impact,SO Term<br />';
-        response.result.forEach(function(res) {
-            pre.innerHTML += res.transcript + ',' + res.impact + ',' + res.so_term + '<br />';
-        });
-    })
-    .catch(function(response) {
-        var pre = document.getElementById('effects');
-        pre.innerHTML = '';
-        pre.innerHTML += response.detail;
+  .then(function(response) {
+    var pre = document.getElementById('effects');
+    pre.innerHTML = '';
+    pre.innerHTML += 'Transcript,Impact,SO Term<br />';
+    response.result.forEach(function(res) {
+      pre.innerHTML += res.transcript + ',' + res.impact + ',' + res.so_term + '<br />';
     });
+  })
+  .catch(function(response) {
+    var pre = document.getElementById('effects');
+    pre.innerHTML = '';
+    pre.innerHTML += response.detail;
+  });
